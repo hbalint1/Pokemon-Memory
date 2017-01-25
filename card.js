95c58284b51e08ffa3843e45e2e8b152667e0fd1
hbalint1 <hobalint94@gmail.com>
2017-01-25 23:22:20 +0100

    adding flip
    Added flip.

--- card.js
+++ card.js
@@ -1,17 +1,25 @@
 class Card {
-  constructor(x_, y_){
+  constructor(x_, y_, img_){
     this.x = x_;
     this.y = y_;
-    this.a = 50;
+    this.a = 80;
+    this.img = img_;
+    this.flipped = false;
   }
 
   flip(){
-
+    this.flipped = !this.flipped;
   }
 
   display(){
-    noFill();
-    stroke(255);
-    rect(this.x, this.y, this.a, this.a);
+    if(this.flipped){
+      noFill();
+      stroke(255);
+      rect(this.x, this.y, this.a, this.a);
+    } else {
+      fill(0);
+      noStroke();
+      rect(this.x, this.y, this.a, this.a);
+    }
   }
 }
--- sketch.js
+++ sketch.js
@@ -14,7 +14,7 @@ function setup() {
   deck = [];
   for(var i = 0; i < mapSize ; i++){
     for(var j = 0; j < mapSize ; j++){
-      deck.push(new Card(i * 80, j * 80));
+      deck.push(new Card(i * 90,j * 90));
     }
   }
 }
@@ -27,12 +27,24 @@ function draw() {
 }
 
 function drawMap() {
-  // for(var i = 0; i < deck.length; i++){
-  //   deck[i].display();
+  for(var i = 0; i < deck.length; i++){
+    deck[i].display();
+  }
+  // for(var i = 0; i < mapSize ; i++){
+  //   for(var j = 0; j < mapSize ; j++){
+  //     image(img, i * 80, j * 80, 80, 80, i * 90, j * 90, 80, 80);
+  //   }
   // }
-  for(var i = 0; i < mapSize ; i++){
-    for(var j = 0; j < mapSize ; j++){
-      image(img, i * 80, j * 80, 80, 80, i * 90, j * 90, 80, 80);
-    }
+}
+
+function mousePressed(){
+  for(var i = 0; i < deck.length; i++){
+    if(mouseX <= deck[i].x + deck[i].a &&
+      mouseX >= deck[i].x &&
+      mouseY <= deck[i].y + deck[i].a &&
+      mouseY >= deck[i].y){
+        // print(i);
+        deck[i].flip();
+      }
   }
 }
